351ddacfbe1f5277ed245e2dfe63aa02b7a80df9
Pedro Yaya <pedrogabrielyaya92@gmail.com>
2018-05-26 12:33:27 -0300

    recylcer parte 1

---
+++ pedroultimoentregable/app/src/main/java/com/example/usuario/pedroultimoentregable/AdapterCuadros.java
@@ -0,0 +1,77 @@
+package com.example.usuario.pedroultimoentregable;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.security.cert.Certificate;
+import java.util.List;
+
+public class AdapterCuadros extends RecyclerView.Adapter {
+
+    private List<Cuadro> listaDeCuadros;
+    private Notificable notificable;
+
+    public AdapterCuadros(List<Cuadro> listaDeCuadros, Notificable notificable) {
+        this.listaDeCuadros = listaDeCuadros;
+        this.notificable = notificable;
+    }
+    
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View viewCelda = layoutInflater.inflate(R.layout.celdarecycler, parent,false);
+        ViewHolderCuadros viewHolderCuadros = new ViewHolderCuadros(viewCelda);
+
+
+        return viewHolderCuadros;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        Cuadro cuadro = listaDeCuadros.get(position);
+        ViewHolderCuadros viewHolderCuadros = (ViewHolderCuadros) holder;
+        viewHolderCuadros.setearDatos(cuadro);
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaDeCuadros.size();
+    }
+
+
+    private class ViewHolderCuadros extends RecyclerView.ViewHolder {
+        private ImageView imagen;
+        private TextView titulo;
+        private TextView descripcion;
+
+
+
+        public ViewHolderCuadros(View itemView) {
+            super(itemView);
+            imagen = itemView.findViewById(R.id.imagenCelda);
+            titulo = itemView.findViewById(R.id.tituloCelda);
+            descripcion = itemView.findViewById(R.id.descripcionCelda);
+
+        }
+
+        public void setearDatos(Cuadro cuadro){
+            imagen.setImageResource(cuadro.getImagen());
+            titulo.setText(cuadro.getTitulo());
+            descripcion.setText(cuadro.getDescripcion());
+        }
+    }
+
+
+    public interface Notificable{
+        void abrirDetalleReceta(List<Cuadro> listaDeCuadros, Integer posicionCuadro);
+    }
+
+
+}
--- pedroultimoentregable/app/src/main/java/com/example/usuario/pedroultimoentregable/FragmentHome.java
+++ pedroultimoentregable/app/src/main/java/com/example/usuario/pedroultimoentregable/FragmentHome.java
@@ -3,9 +3,13 @@ package com.example.usuario.pedroultimoentregable;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -13,6 +17,8 @@ import android.view.ViewGroup;
  */
 public class FragmentHome extends Fragment {
 
+    private AdapterCuadros adapterCuadros;
+    private AdapterCuadros.Notificable notificable;
 
     public FragmentHome() {
         // Required empty public constructor
@@ -23,7 +29,52 @@ public class FragmentHome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        RecyclerView recyclerViewCuadros = view.findViewById(R.id.recyclerViewPrincipal);
+        recyclerViewCuadros.setHasFixedSize(true);
+
+        adapterCuadros = new AdapterCuadros(crearListaCuadros(), )
+
+
+        return view;
+    }
+
+
+    private static List<Cuadro> crearListaCuadros() {
+
+        List<Cuadro> listaCuadros = new ArrayList<>();
+
+        for (int i = 0; i < 2; i++) {
+            listaCuadros.add(new Cuadro(R.drawable.ata, "ataaa", "asdasadasdadadasdad"));
+            listaCuadros.add(new Cuadro(R.drawable.ata, "ataaa", "asdasadasdadadasdad"));
+            listaCuadros.add(new Cuadro(R.drawable.ata, "ataaa", "asdasadasdadadasdad"));
+            listaCuadros.add(new Cuadro(R.drawable.ata, "ataaa", "asdasadasdadadasdad"));
+            listaCuadros.add(new Cuadro(R.drawable.ata, "ataaa", "asdasadasdadadasdad"));
+            listaCuadros.add(new Cuadro(R.drawable.ata, "ataaa", "asdasadasdadadasdad"));
+
+
+        }
+
+        return listaCuadros;
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
