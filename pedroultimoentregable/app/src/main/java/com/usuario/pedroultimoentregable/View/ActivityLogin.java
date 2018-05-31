9d4dd4d6f89ff3276e18373f0f203c8024053982
Pedro Yaya <pedrogabrielyaya92@gmail.com>
2018-05-30 21:21:48 -0300

    cambio a activity login

---
+++ pedroultimoentregable/app/src/main/java/com/usuario/pedroultimoentregable/View/ActivityLogin.java
@@ -0,0 +1,15 @@
+package com.usuario.pedroultimoentregable.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.usuario.pedroultimoentregable.R;
+
+public class ActivityLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
