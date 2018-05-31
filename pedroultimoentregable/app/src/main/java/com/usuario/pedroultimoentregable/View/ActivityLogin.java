d8e097eeec8c968e8db88fc569c79daab9f9da39
Pedro Yaya <pedrogabrielyaya92@gmail.com>
2018-05-30 21:50:06 -0300

    login firebase

--- pedroultimoentregable/app/src/main/java/com/usuario/pedroultimoentregable/View/ActivityLogin.java
+++ pedroultimoentregable/app/src/main/java/com/usuario/pedroultimoentregable/View/ActivityLogin.java
@@ -2,22 +2,43 @@ package com.usuario.pedroultimoentregable.View;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.usuario.pedroultimoentregable.R;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.usuario.pedroultimoentregable.Model.Cuadro;
+
+import java.util.List;
 
 public class ActivityLogin extends AppCompatActivity {
 
 
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private CallbackManager mCallbackManager;
 
+
+
+    private EditText editTextMail;
+    private EditText editTextPass;
+    private Button botonCrear;
+    private Button botonLogin;
 
 
     @Override
@@ -25,12 +46,143 @@ public class ActivityLogin extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
 
 
+        editTextMail = findViewById(R.id.editTextEmail);
+        editTextPass = findViewById(R.id.editTextContraseña);
+        botonCrear = findViewById(R.id.BotonCrearCuenta);
+        botonLogin = findViewById(R.id.BotonIniciarSesion);
+
+
+
+
+        botonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUsuario(editTextMail.getText().toString(), editTextPass.getText().toString());
+            }
+        });
+
+
+        botonCrear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                crearUsuario(editTextMail.getText().toString(), editTextPass.getText().toString());
+            }
+        });
+
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("facebook", "onAuthStateChanged:signed_out");
+
+
+                } else {
+                    // User is signed out
+                    Log.d("facebook", "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
 
 
 
     }
+
+
+    private void loginUsuario(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Intent intent = new Intent(ActivityLogin.this,MainActivity.class);
+                            startActivity(intent);
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(ActivityLogin.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
+
+
+
+
+
+
+    private void crearUsuario(String email, String password){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("firebase", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("firebase", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(ActivityLogin.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
+
+  /*  @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }*/
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    }
+
+
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Pass the activity result back to the Facebook SDK
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+
+
+
+
+
 
 
 
